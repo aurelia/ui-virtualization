66cb4f79ca1acd515817575f89fe92e0d3cfa6c3
Martin Gustafsson <gusten86@gmail.com>
2015-07-16 11:00:00 +0200

    chore(all): include html templates in build
    Closes #2

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -28,10 +28,18 @@ gulp.task('build-system', function () {
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
+gulp.task('build-html', function () {
+  return gulp.src(paths.html)
+    .pipe(gulp.dest(paths.output + 'es6'))
+    .pipe(gulp.dest(paths.output + 'commonjs'))
+    .pipe(gulp.dest(paths.output + 'amd'))
+    .pipe(gulp.dest(paths.output + 'system'));
+});
+
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
-    ['build-es6', 'build-commonjs', 'build-amd', 'build-system'],
+    ['build-es6', 'build-commonjs', 'build-amd', 'build-system', 'build-html'],
     callback
   );
 });
