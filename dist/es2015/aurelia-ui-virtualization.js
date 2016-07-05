7c04ecebc84c9960b3686c438155299c889708d4
Andrew Stoker <TheAndyBob@me.com>
2016-07-05 09:21:11 -0400

    refactor(dist): exclude dist folder

--- dist/es2015/aurelia-ui-virtualization.js
+++
@@ -1,8 +0,0 @@
-import { VirtualRepeat } from './virtual-repeat';
-import { VirtualRepeatNext } from './virtual-repeat-next';
-
-export function configure(config) {
-  config.globalResources('./virtual-repeat', './virtual-repeat-next');
-}
-
-export { VirtualRepeat, VirtualRepeatNext };
