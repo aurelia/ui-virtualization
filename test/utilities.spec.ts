6dac2ba899ddff8adfd7e6f08668fedf91e8a5ee
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-03-09 00:29:25 +1100

    chore(tests): more spacing calc tests

--- test/utilities.spec.ts
+++ test/utilities.spec.ts
@@ -1,6 +1,7 @@
 import './setup';
 import { eachCartesianJoin } from "./lib";
 import { getDistanceToParent } from "../src/utilities-dom";
+import { PLATFORM } from 'aurelia-pal';
 
 describe('utiltites - DOM', () => {
   
@@ -11,11 +12,14 @@ describe('utiltites - DOM', () => {
         <div id="topBuffer" style="height: 500px;"></div>
         <div id="child"></div>
       </div>`,
+      spaceAssertions: [
+        ['#child', '#parent', 500],
+        ['#topBuffer', '#parent', 0]
+      ],
       assert: (ct) => {
         const parent = ct.querySelector('#parent') as HTMLElement;
         const topBuffer = ct.querySelector('#topBuffer') as HTMLElement;
         const child = ct.querySelector('#child') as HTMLElement;
-        expect(getDistanceToParent(child, parent)).toEqual(500);
         topBuffer.style.height = '1px';
         expect(getDistanceToParent(child, parent)).toEqual(1);
         ct.style.marginTop = '-50px';
@@ -29,17 +33,78 @@ describe('utiltites - DOM', () => {
         <div id="topBuffer" style="height: 500px;"></div>
         <div id="child"></div>
       </div>`,
+      spaceAssertions: [
+        ['#child', '#parent', 500],
+      ],
       assert: ct => {
         const parent = ct.querySelector('#parent') as HTMLElement;
         const topBuffer = ct.querySelector('#topBuffer') as HTMLElement;
         const child = ct.querySelector('#child') as HTMLElement;
-        expect(getDistanceToParent(child, parent)).toEqual(500);
         topBuffer.style.height = '1px';
         expect(getDistanceToParent(child, parent)).toEqual(1);
         ct.style.marginTop = '-50px';
         expect(getDistanceToParent(child, parent)).toEqual(1);
         topBuffer.insertAdjacentHTML('afterend', '<div style="height: 500px"></div>');
         expect(getDistanceToParent(child, parent)).toEqual(501);
+      }
+    },
+    {
+      title: 'parent[relative + scrolling] + buffer + child',
+      template:
+      `<div id="parent" style="position: relative; height: 200px; overflow-y: auto">
+        <div style="height: 300px"></div>
+        <div id="topBuffer" style="height: 400px"></div>
+        <div id="child"></div>
+      </div>`,
+      spaceAssertions: [
+        ['#child', '#parent', 700],
+        ['#topBuffer', '#parent', 300]
+      ]
+    },
+    {
+      title: 'scroller > table > row',
+      template: `<div id="parent" style="height: 300px; overflow-y: auto">
+        <div style="height: 400px"></div>
+        <table style="border-spacing: 0">
+          <tr id="topBuffer" style="height: 600px"></tr>
+          <tbody>
+            <tr id="child"></tr>
+          </tbody>
+        </table>
+      </div>`,
+      spaceAssertions: [
+        ['#child', '#parent', 1000],
+        ['#topBuffer', '#parent', 400]
+      ],
+      assert: ct => {
+        ct.scrollTop = 600;
+        assertDistance(ct, '#child', '#parent', 1000);
+        assertDistance(ct, '#topBuffer', '#parent', 400);
+      }
+    },
+    {
+      title: 'scroller > div[relative] > div > table > row',
+      template: `<div id="parent" style="height: 300px; overflow-y: auto">
+        <div style="height: 400px"></div>
+        <div style="position: relative; height: 400px;">
+          <div>
+            <table style="border-spacing: 0">
+              <tr id="topBuffer" style="height: 600px"></tr>
+              <tbody>
+                <tr id="child"></tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>`,
+      spaceAssertions: [
+        ['#child', '#parent', 1000],
+        ['#topBuffer', '#parent', 400]
+      ],
+      assert: ct => {
+        ct.scrollTop = 800;
+        assertDistance(ct, '#child', '#parent', 1000);
+        assertDistance(ct, '#topBuffer', '#parent', 400);
       }
     },
     {
@@ -61,9 +126,9 @@ describe('utiltites - DOM', () => {
         const table = ct.querySelector('#table') as HTMLElement;
         const child = ct.querySelector('#child') as HTMLElement;
         const cell = ct.querySelector('#cell') as HTMLElement;
-        expect(getDistanceToParent(child, table)).toEqual(500);
-        expect(getDistanceToParent(topBuffer, table)).toEqual(0);
-        expect(getDistanceToParent(cell, table)).toEqual(500);
+        assertDistance(ct, child, table, 500);
+        assertDistance(ct, topBuffer, table, 0);
+        assertDistance(ct, cell, table, 500);
         
         expect(getDistanceToParent(child, parent)).toEqual(500);
         expect(getDistanceToParent(topBuffer, parent)).toEqual(0);
@@ -84,12 +149,13 @@ describe('utiltites - DOM', () => {
   interface ITestCase {
     title: string;
     template: string | HTMLElement | HTMLElement[];
-    assert: (ct: HTMLDivElement) => void;
+    spaceAssertions?: Array<[/*child selector*/string, /*parent selector*/string, /*supposed distance*/number]>
+    assert?: (ct: HTMLDivElement) => void;
   }
 
   eachCartesianJoin(
     [testCases],
-    ({ title, template, assert }) => {
+    ({ title, template, spaceAssertions = [], assert = PLATFORM.noop }) => {
       it(title, () => {
         const container = document.createElement('div');
         if (typeof template === 'string') {
@@ -100,9 +166,24 @@ describe('utiltites - DOM', () => {
           container.appendChild(template.cloneNode(true));
         }
         document.body.appendChild(container);
-        assert(container);
-        document.body.removeChild(container);
+        try {
+          for (const [childSelector, parentSelector, distance] of spaceAssertions) {
+            assertDistance(container, childSelector, parentSelector, distance);
+          }
+          assert(container);
+        } catch ($ex) {
+          throw $ex;
+        } finally {
+          document.body.removeChild(container);
+        }
       });
     }
   );
+
+  const assertDistance = (ct: HTMLElement, child: string | HTMLElement, parent: string | HTMLElement, distance: number) => {
+    expect(getDistanceToParent(
+      typeof child === 'string' ? ct.querySelector(child) : child,
+      typeof parent === 'string' ? ct.querySelector(parent) : parent,
+    )).toEqual(distance, `${typeof child === 'string' ? child : child.tagName} <--${distance}px--> ${typeof parent === 'string' ? parent : parent.tagName}`);
+  }
 });
