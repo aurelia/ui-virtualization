3156a12049176886c29c30f0d253a5823f6ac751
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-03-13 01:16:36 +1100

    chore(examples): tweak build tsconfig, add placeholder for 117

---
+++ sample/sample-v-ui-app/src/issue-117/sub-app.ts
@@ -0,0 +1,55 @@
+interface IScrollContext {
+  isAtTop: boolean;
+  isAtBottom: boolean;
+  topIndex: number;
+}
+
+export interface Person {
+  fname: string;
+  lname: string;
+}
+
+const fNames = [
+  // tslint:disable-next-line:max-line-length
+  'Ford', 'Arthur', 'Trillian', 'Sneezy', 'Sleepy', 'Dopey', 'Doc', 'Happy', 'Bashful', 'Grumpy', 'Mufasa', 'Sarabi', 'Simba', 'Nala', 'Kiara', 'Kovu', 'Timon', 'Pumbaa', 'Rafiki', 'Shenzi'
+];
+const lNames = [
+  // tslint:disable-next-line:max-line-length
+  'Prefect', 'Dent', 'Astra', 'Adams', 'Baker', 'Clark', 'Davis', 'Evans', 'Frank', 'Ghosh', 'Hills', 'Irwin', 'Jones', 'Klein', 'Lopez', 'Mason', 'Nalty', 'Ochoa', 'Patel', 'Quinn', 'Reily', 'Smith', 'Trott', 'Usman', 'Valdo', 'White', 'Xiang', 'Yakub', 'Zafar'
+];
+export class App {
+  private people: Person[];
+
+  constructor() {
+    this.people = [
+      { fname: fNames[0], lname: lNames[0] },
+      { fname: fNames[1], lname: lNames[1] },
+      { fname: fNames[2], lname: lNames[2] }
+    ];
+    this.push30(undefined, 5);
+  }
+
+  public push30(scrollContext?: IScrollContext, count = 30) {
+    console.log('Issue-129 getting more...');
+    // if (scrollContext) {
+    //   console.log('Issue-129 getting more:', JSON.stringify(scrollContext, undefined, 2));
+    // }
+    if (!scrollContext || scrollContext.isAtBottom) {
+      for (let i = 0; i < count; i++) {
+        this.people.push({
+          fname: fNames[Math.floor(Math.random() * fNames.length)],
+          lname: lNames[Math.floor(Math.random() * lNames.length)]
+        });
+      }
+    }
+    else if (scrollContext && scrollContext.isAtTop) {
+      for (let i = 0; i < count; i++) {
+        this.people.unshift({
+          fname: fNames[Math.floor(Math.random() * fNames.length)],
+          lname: lNames[Math.floor(Math.random() * lNames.length)]
+        });
+      }
+    }
+    console.log('Population size:', this.people.length);
+  }
+}
